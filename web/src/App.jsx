0851c2cf0ff38d7070036c2f82a46356bf2fe7a3
Tayyaba-Ali20 <ta6402792@gmail.com>
2023-03-12 22:26:52 +0500

    done

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
+import Cart from './pages/Cart/Cart';
 import Home from './pages/Home/Home';
 import Welcome from './pages/Welcome/Welcome';
 // import HomeScreen from './screens/HomeScreen';
@@ -18,6 +19,7 @@ function App() {
 						<Route path='/signup' element={<SignUp />} />
 						<Route path='/sign-in' element={<SignIn />} />
 						<Route path='/products' element={<Home />} />
+						<Route path='/cart' element={<Cart />} />
 						{/* <Route path='/profile' element={<Profile />} /> */}
 					</Routes>
 				</Router>
